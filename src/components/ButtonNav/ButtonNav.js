9cee7ff04c663640a5c6185a7e12389c2b5d6368
Mohammad Abdullah <bsef19a531@pucit.edu.pk>
2022-07-28 22:21:51 +0500

    Added onClick ButtonNav CSS

--- src/components/ButtonNav/ButtonNav.js
+++ src/components/ButtonNav/ButtonNav.js
@@ -2,14 +2,32 @@ import styles from "./ButtonNav.module.css";
 
 const ButtonNav = () => {
 
+    const navButtonClickHandler = (id) => {
 
+        if (id === "keypad") {
+            document.getElementById("recents").classList.remove(styles.active_button);
+            document.getElementById("contacts").classList.remove(styles.active_button);
+        }
+        else if (id === "recents") {
+            document.getElementById("keypad").classList.remove(styles.active_button);
+            document.getElementById("contacts").classList.remove(styles.active_button);
+        }
+        else {
+            document.getElementById("recents").classList.remove(styles.active_button);
+            document.getElementById("keypad").classList.remove(styles.active_button);
+        }
+
+        const elem = document.getElementById(id);
+        elem.classList.add(styles.active_button);
+
+    }
 
     return (
         <div className={styles.nav}>
             <div className={styles.nav_container}>
-                <button id="keypad">Keypad</button>
-                <button id="recents">Recents</button>
-                <button id="contacts">Contacts</button>
+                <button className={styles.nav_container_button} id="keypad" onClick={() => { navButtonClickHandler("keypad") }}>Keypad</button>
+                <button className={styles.nav_container_button} id="recents" onClick={() => { navButtonClickHandler("recents") }}>Recents</button>
+                <button className={styles.nav_container_button} id="contacts" onClick={() => { navButtonClickHandler("contacts") }}>Contacts</button>
             </div>
         </div>
     )
